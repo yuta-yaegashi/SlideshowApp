fd2907e40137840f1cf97e4d11c8b6dd0f7ef170
yuta.yaegashi <yuta090711@icloud.com>
2021-04-30 07:48:49 +0900

    画面遷移ができるようにした

---
+++ SlideshowApp/ResultViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ResultViewController.swift
+//  SlideshowApp
+//
+//  Created by 八重樫裕太 on 2021/04/30.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    @IBOutlet weak var UIImage: UIImageView!
+    
+    //受け取るための変数
+    var x = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -9,8 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+           
+    }
+
     @IBOutlet weak var startButton: UIButton!
     @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var backButton: UIButton!
     
     // 配列に指定するindex番号を宣言
         var nowIndex:Int = 0
@@ -29,6 +35,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        // indexの画像をstoryboardの画像にセットする
+        image.image = imageArray[nowIndex]
+
     }
     
     // 再生ボタンを押した時の処理
@@ -44,6 +53,12 @@ class ViewController: UIViewController {
 
                 // ボタンの名前を停止に変える
                 startButton.setTitle("停止", for: .normal)
+                
+                //進むボタンを非表示にする
+                nextButton.isHidden = true
+                
+                //戻るボタンを非表示にする
+                backButton.isHidden = true
 
             } else {
                 // 停止時の処理を実装
@@ -54,10 +69,19 @@ class ViewController: UIViewController {
                 timer = nil
 
                            // ボタンの名前を再生に直しておく
-                           startButton.setTitle("停止", for: .normal)
+                           startButton.setTitle("再生", for: .normal)
+                
+                //進むボタンを表示する
+                nextButton.isHidden = false
+                
+                //戻るボタンを表示する
+                backButton.isHidden = false
                        }
                    }
     @objc func changeImage() {
+        
+       
+
             // indexを増やして表示する画像を切り替える
             nowIndex += 1
 
@@ -66,8 +90,49 @@ class ViewController: UIViewController {
                 // indexを一番最初の数字に戻す
                 nowIndex = 0
             }
-            // indexの画像をstoryboardの画像にセットする
-            image.image = imageArray[nowIndex]
+        // indexの画像をstoryboardの画像にセットする
+        image.image = imageArray[nowIndex]
         }
+ //進むボタンを押したときの処理
+    @IBAction func nextButton(_ sender: Any) {
+        
+      
+
+            // indexを増やして表示する画像を切り替える
+            nowIndex += 1
+
+            // indexが表示予定の画像の数と同じ場合
+        if (nowIndex == imageArray.count) {
+                // indexを一番最初の数字に戻す
+                nowIndex = 0
+            }
+        // indexの画像をstoryboardの画像にセットする
+        image.image = imageArray[nowIndex]
+        
+    }
+    @IBAction func backButton(_ sender: Any) {
+       
+        // 表示されている画像が先頭の時
+        if (nowIndex == 0) {
+            // indexを一番最後の数字に戻す
+            nowIndex = imageArray.count
+        }
+            // indexを減らして表示する画像を切り替える
+            nowIndex -= 1
+
+           
+        // indexの画像をstoryboardの画像にセットする
+        image.image = imageArray[nowIndex]
+        
+    }
+    
+    @IBAction func tappAction(_ sender: Any) {
+    }
+    
+   
+    //遷移先から戻ってきた時のアクション
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
+        }
+    
 }
 
